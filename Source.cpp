360e02f46ac56b9283a63948cc28c0c1b7a7876e
FaustasZil <95297355+FaustasZil@users.noreply.github.com>
2022-02-21 01:58:36 +0200

    Atnaujinta v0.1 versija

--- Source.cpp
+++ Source.cpp
@@ -11,21 +11,25 @@ using std::string;
 struct zmogus {
 	string vardas = "", pavarde = "";
 	int paz[N] = { 0 }, egz = 0, ndskc =0;
+	std::vector<int> vpaz;
 	double rezult = 0;
 	double median = 0;
 };
 void ivestis(zmogus& temp);
-void isvestis(zmogus& temp);
+void isvestis(zmogus& temp, bool vm);
 int main() {
 	zmogus a;
 	zmogus* amas = new zmogus[M];
+	bool vm;
 	for (int i = 0; i < M; i++) {
 		ivestis(amas[i]);
 	}
-	cout << "|" << std::left << std::setw(20) << "Pavarde" << "|" << std::left << std::setw(20) << "Vardas" << "|" << std::left << std::setw(20) << "Galutinis (vid.)" << "|" << std::left << std::setw(20) << "Galutinis (med.)";
+	cout << "Galutini pazymi skaiciuoti pagal vidurki(0) ar pagal mediana(1): ";
+	cin >> vm;
+	cout << std::endl<< "|" << std::left << std::setw(20) << "Pavarde" << "|" << std::left << std::setw(20) << "Vardas" << "|" << std::left << std::setw(20) << "Galutinis (vid.)/Galutinis (med.)";
 	cout << std::endl << "---------------------------------------------------" << std::endl;
 	for (int i = 0; i < M; i++) {
-		isvestis(amas[i]);
+		isvestis(amas[i], vm);
 	}
 	delete[] amas;
 	system("pause");
@@ -33,42 +37,42 @@ int main() {
 void ivestis(zmogus& temp) {
 	int a = 0;
 	int check = 1;
+	temp.vpaz.reserve(100);
 	cout << "Iveskite varda: ";
 	cin >> temp.vardas;
 	cout << "Iveskite pavarde: ";
 	cin >> temp.pavarde;
 	while (true) {
-		cout << "Iveskite " << a+1 << "-a(-i) pazymi(jei norite baigti, spauskite 0):";
+		cout << "Iveskite " << temp.vpaz.size() + 1 << "-a(-i) pazymi(jei norite baigti, spauskite 0):";
 		cin >> check;
 		if (check == 0)
 			break;
 		else {
-			temp.paz[a] = check;
-			temp.rezult += temp.paz[a];
-			a++;
+			temp.vpaz.push_back(check);
+			temp.rezult += check;
 		}
 	}
 	cout << "Iveskite egzamino ivertinima: ";
 	cin >> temp.egz;
 	temp.rezult += temp.egz;
-	temp.paz[a] = temp.egz;
-	a++;
+	temp.vpaz.push_back(temp.egz);
+	a = temp.vpaz.size();
 	temp.ndskc = a;
 
-	std::vector<int> vekpaz(temp.paz, temp.paz + a);
-	std::sort (vekpaz.begin(), vekpaz.end());
+	std::sort (temp.vpaz.begin(), temp.vpaz.end());
 	//for (auto it = vekpaz.begin(); it != vekpaz.end(); ++it)
 	//	cout << " " << *it;
 	if (a % 2 != 0)
-		temp.median = vekpaz[(a / 2)];
+		temp.median = temp.vpaz[(a / 2)];
 	else
-		temp.median = (vekpaz[(a / 2)-1] + vekpaz[a / 2]) / 2.0;
+		temp.median = (temp.vpaz[(a / 2)-1] + temp.vpaz[a / 2]) / 2.0;
 }
-void isvestis(zmogus& temp) {
+void isvestis(zmogus& temp, bool vm) {
 	cout << "|" << std::left << std::setw(20) << temp.vardas << "|" << std::left <<
 		std::setw(20) << temp.pavarde << std::right;
-	//for (int i = 0; i < 3; i++)
-	//	cout << "|" << std::setw(5) << temp.paz[i];
-	cout << "|" << std::left << std::setw(20) << temp.rezult/(temp.ndskc);
-	cout << "|" << std::left << std::setw(20) << temp.median << "|" << std::endl;
+	if (vm) {
+		cout << "|" << std::left << std::setw(20) << temp.median << "|" << std::endl;
+	}
+	else
+		cout << "|" << std::left << std::setw(20) << temp.rezult/(temp.ndskc);
 }
