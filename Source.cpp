50310141447d87a3e8aab4f26d2144a390b012a7
FaustasZil <95297355+FaustasZil@users.noreply.github.com>
2022-03-07 23:18:22 +0200

    v0.3 versija su pridetu failu handlinimu

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,8 @@
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-#include <vector>
-#include <random>
-#include <fstream>
-#include <sstream>
+#include "headeris.h"
 using std::cout;
 using std::cin;
 using std::string;
-struct zmogus {
-	string vardas = "", pavarde = "";
-	int egz = 0, ndskc =0;
-	std::vector<int> vpaz;
-	double rezult = 0;
-	double median = 0;
-};
-void ivestis(zmogus& temp);
-void isvestis(zmogus& temp, int vm);
-void fisvestis(std::vector <zmogus> temp, int vm);
-bool sortf(zmogus pirmas, zmogus antras);
+
 int main() {
 	string laikinas = "";
 	int M, N;
@@ -43,9 +26,15 @@ int main() {
 	}
 	if (N == 1) {
 		std::stringstream bufferis;
-		std::ifstream open_f("kursiokai.txt");
-		bufferis << open_f.rdbuf();
-		open_f.close();
+		try {
+			std::ifstream open_f("kursiokai.txt");
+			bufferis << open_f.rdbuf();
+			open_f.close();
+		}
+		catch (...) {
+			cout << "Pasirinkto duomenu failo nera. Programa uzbaigta";
+			return 0;
+		}
 		string temp;
 		zmogus ztemp;
 		int itemp;
@@ -116,145 +105,17 @@ int main() {
 		fisvestis(amas, vm);
 	}
 	else {
-		cout << std::endl << "|" << std::left << std::setw(20) << "Vardas" << "|" << std::left << std::setw(20) << "Pavarde" << "|" << std::left << std::setw(20) << "Galutinis (vid.)/Galutinis (med.)";
-		cout << std::endl << "---------------------------------------------------" << std::endl;
+		if (vm == 1) {
+			cout << std::endl << "|" << std::left << std::setw(20) << "Vardas" << "|" << std::left << std::setw(20) << "Pavarde" << "|" << std::left << std::setw(20) << "Galutinis (med.)" << "|";
+			cout << std::endl << "----------------------------------------------------------------" << std::endl;
+		}
+		else {
+			cout << std::endl << "|" << std::left << std::setw(20) << "Vardas" << "|" << std::left << std::setw(20) << "Pavarde" << "|" << std::left << std::setw(20) << "Galutinis (vid.)" << "|";
+			cout << std::endl << "----------------------------------------------------------------" << std::endl;
+		}
 		for (int i = 0; i < M; i++) {
 			isvestis(amas[i], vm);
 		}
 	}
 	system("pause");
-}
-void ivestis(zmogus& temp) {
-	int a = 0;
-	string laikinas;
-	int check = 1;
-	int rd;
-	int temprando;
-	cout << "Iveskite varda: ";
-	cin >> temp.vardas;
-	cout << "Iveskite pavarde: ";
-	cin >> temp.pavarde;
-	std::getline(cin, laikinas);
-	while (true) {
-		cout << "Ar norite automatiskai sugeneruoti pazymius(1 taip, 0 ne)? ";
-		std::getline(cin, laikinas);
-		try {
-			rd = stoi(laikinas);
-			if (rd != 0 && rd != 1) {
-				cout << "Ivestas ne tas pasirinkimas" << std::endl;
-				continue;
-			}
-			break;
-		}
-		catch (...) {
-			cout << "Ivestas ne tas pasirinkimas" << std::endl;
-		}
-	}
-	if (rd==1) {
-		std::random_device rd;
-		std::mt19937 mt(rd());
-		while (true) {
-			cout << "Kiek pazymiu sugeneruoti? ";
-			try {
-				std::getline(cin, laikinas);
-				a = stoi(laikinas);
-				break;
-			}
-			catch (...) {
-				cout << "Neteisingas kiekis" << std::endl;
-			}
-		}
-		std::uniform_int_distribution<int> dist(1, 10);
-		for (int i = 0; i < a; i++) {
-			temprando = dist(mt);
-			temp.vpaz.push_back(temprando);
-			temp.rezult += temprando;
-		}
-		temprando = dist(mt);
-		temp.egz = temprando;
-	}
-	else {
-		while (true) {
-			cout << "Iveskite " << temp.vpaz.size() + 1 << "-a(-i) pazymi(jei norite baigti, spauskite 0):";
-			try {
-				std::getline(cin, laikinas);
-				check = stoi(laikinas);
-			}
-			catch (...) {
-				cout << "Ivestas ne tas pasirinkimas" << std::endl;
-				continue;
-			}
-			if (check == 0)
-				break;
-			else if (check < 0 || check >10) {
-				cout << "Ivestas ne tas pasirinkimas" << std::endl;
-				continue;
-			}
-			else {
-				temp.vpaz.push_back(check);
-				temp.rezult += check;
-			}
-		}
-		while (true) {
-			cout << "Iveskite egzamino ivertinima: ";
-			try {
-				std::getline(cin, laikinas);
-				temp.egz = stoi(laikinas);
-				if (temp.egz <= 0 || temp.egz > 10) {
-					cout << "Ivestas ne tas pasirinkimas" << std::endl;
-					continue;
-				}
-				break;
-			}
-			catch (...) {
-				cout << "Ivestas ne tas pasirinkimas" << std::endl;
-			}
-		}
-	}
-	
-	a = temp.vpaz.size();
-	temp.ndskc = a;
-
-	std::sort(temp.vpaz.begin(), temp.vpaz.end());
-	if (a % 2 != 0)
-		temp.median = temp.vpaz[(a / 2)];
-	else
-		temp.median = (temp.vpaz[(a / 2)-1] + temp.vpaz[a / 2]) / 2.0;
-}
-void isvestis(zmogus& temp, int vm) {
-	cout << "|" << std::left << std::setw(20) << temp.vardas << "|" << std::left <<
-		std::setw(20) << temp.pavarde << std::right;
-	if (vm==1) {
-		cout << "|" << std::left << std::setw(20) << temp.median*0.4+temp.egz*0.6 << "|" << std::endl;
-	}
-	else
-		cout << "|" << std::left << std::setw(20) << (temp.rezult/(temp.ndskc))*0.4+temp.egz*0.6<< std::endl;
-}
-void fisvestis(std::vector <zmogus> temp, int vm) {
-	std::ofstream open_f("output.txt");
-	open_f << std::endl << "|" << std::left << std::setw(20) << "Vardas" << "|" << std::left << std::setw(20) << "Pavarde" << "|";
-	std::stringstream rasbufferis;
-	if (vm == 1) {
-		open_f << std::left << std::setw(20) << "Galutinis (med.)"<<"|";
-		open_f << std::endl << "----------------------------------------------------------------" << std::endl;
-		for (int i = 0; i < temp.size(); i++) {
-			rasbufferis << "|" << std::left << std::setw(20) << temp[i].vardas << "|" << std::left <<
-				std::setw(20) << temp[i].pavarde << std::right << "|" << std::left << std::setw(20) << temp[i].median * 0.4 + temp[i].egz * 0.6 << "|" << std::endl;
-		}
-	}
-	else {
-		open_f << std::left << std::setw(20) << "Galutinis (vid.)" << "|";
-		open_f << std::endl << "----------------------------------------------------------------" << std::endl;
-		for (int i = 0; i < temp.size(); i++) {
-			rasbufferis << "|" << std::left << std::setw(20) << temp[i].vardas << "|" << std::left <<
-				std::setw(20) << temp[i].pavarde << std::right << "|" << std::left << std::setw(20) << (temp[i].rezult / (temp[i].ndskc)) * 0.4 + temp[i].egz * 0.6 << "|" << std::endl;
-		}
-	}
-	open_f << rasbufferis.str();
-	rasbufferis.clear();
-	open_f.close();
-}
-bool sortf(zmogus pirmas, zmogus antras) {
-	if (pirmas.vardas != antras.vardas)
-		return pirmas.vardas < antras.vardas;
 }
---
+++ headeris.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <fstream>
+#include <sstream>
+struct zmogus {
+	std::string vardas = "", pavarde = "";
+	int egz = 0, ndskc = 0;
+	std::vector<int> vpaz;
+	double rezult = 0;
+	double median = 0;
+};
+void ivestis(zmogus& temp);
+void isvestis(zmogus& temp, int vm);
+void fisvestis(std::vector <zmogus> temp, int vm);
+bool sortf(zmogus pirmas, zmogus antras);
